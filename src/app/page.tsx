691bcfe841dc51518d4293c1872526e9352a05ae
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-08-12 15:01:08 +0900

    見出しの文字を作った

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import { MenuIcon } from "lucide-react";
 
 export default function Home() {
-  return <MenuIcon />;
+  return (
+    <>
+      <MenuIcon />
+      <div className="flex flex-col items-center mt-20">
+        <h1 className="text-5xl font-bold">成蹊大学教授図鑑</h1>
+        <p className="mt-3">SEIKEI UNIVERSITY PROFESSOR CATALOG</p>
+      </div>
+    </>
+  );
 }
