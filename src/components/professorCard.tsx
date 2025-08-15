8ec2fe3bf513fa23881f3d48feb131e138f86bce
unknown <sohgo.kobatyan@icloud.com>
2025-08-15 13:55:46 +0900

    修正

--- src/components/professorCard.tsx
+++ src/components/professorCard.tsx
@@ -1,6 +1,4 @@
 "use client";
-import React from "react";
-{/*import Link from "next/link";*/}
 import Image from "next/image";
 
 import { AspectRatio } from "@/components/ui/aspect-ratio";
@@ -10,12 +8,12 @@ const ProfessorCard = () => {
     <div>
       <div className="flex max-w-lg rounded-lg border shadow-md relative">
         {/* プロフィール画像欄 */}
-        <div className="w-40 flex-shrink-0 ">
-          <AspectRatio ratio={10 / 16} className="bg-muted-519 rounded-lg border shadow-md m-2">
+        <div className="w-40 flex-shrink-0">
+          <AspectRatio ratio={9 / 16} className="bg-muted-519 bg-rose-300 rounded-lg border shadow-md m-2">
           <Image 
-            src="/next.svg" 
+            src="/ElenaFuji.png" 
             alt="プロフィール写真" 
-            className="rounded-md object-cover bg-519"
+            className="rounded-md object-contain"
             fill
           />
           </AspectRatio>
@@ -24,20 +22,20 @@ const ProfessorCard = () => {
         <div className="flex-1 p-4">
         <div className="mb-4">
           <h3 className="text-sm font-semibold text-gray-500">名前</h3>
-          <div><h4 className="text-lg font-bold">〇〇〇〇</h4></div>
+          <div><h4 className="text-lg font-semibold">藤井エレナ - Fuji Elena</h4></div>
         </div>
           <div className="mb-4">
           <h3 className="text-sm font-semibold text-gray-500">在籍</h3>
-        <div><h4 className="text-lg font-bold">〇〇〇〇学科</h4></div>
+        <div><h4 className="text-lg font-semibold">現代社会学科</h4></div>
         </div>
         <div className="mb-4">
           <h3 className="text-sm font-semibold text-gray-500">研究テーマ</h3>
-          <div><h4 className="text-lg font-bold">〇〇の〇〇について</h4></div>
+          <div><h4 className="text-lg font-semibold">ナラティブ・サイコロジー</h4></div>
         </div>
         {/* 研究室No */}
-        <div className="absolute bottom-2 right-2 flex-1 flex-col items-end text-white mix-blend-difference">
-            <span className="text-base font-semibold" style={{ writingMode: 'vertical-rl' }}>研究室</span>
-            <span className="text-5xl font-bold">1014</span>
+        <div className="absolute bottom-2 right-2 flex-1 flex-col items-end mix-blend-difference">
+            <span className="text-base text-gray-500 font-semibold" style={{ writingMode: 'vertical-rl' }}>研究室</span>
+            <span className="text-5xl text-gray-500 font-semibold">1014</span>
           </div>
       </div>
     </div>
