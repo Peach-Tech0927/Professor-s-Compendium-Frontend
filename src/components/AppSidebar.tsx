396db2439ce135c1bfd8f9456fb175363d776c96
taroimokun <runa.osada.26@outlook.jp>
2025-08-24 01:13:30 +0900

    不要なコメントアウトの削除

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -1,16 +1,5 @@
-import { AlignJustify } from "lucide-react";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
 
-// const MenuIcon = () => {
-//   return (
-//     <>
-//       <AlignJustify style={{ position: "fixed", top: 10, left: 10 }} />
-//     </>
-//   );
-// };
-
-// export default MenuIcon;
 
 export function AppSidebar() {
   return (
