c7d56e6943e984e6306ecb993c6a30ac39a99876
kentf <kento.fukumoto0222@gmail.com>
2025-08-25 00:33:30 +0900

    Add faculty_header.png and update page.tsx

--- src/app/faculty/name/page.tsx
+++ src/app/faculty/name/page.tsx
@@ -1,5 +1,28 @@
+import { Card, CardContent } from "@/components/ui/card";
+
 const FacultyPage = () => {
-  return <div>ここは学部ページ /faculty/nameでアクセスできる</div>;
+  return(
+    <>
+      <div
+      className="relative h-screen w-full bg-cover bg-center"
+      style={{ backgroundImage: "url('/faculty_header.png')" }}
+      >
+
+        {/* 背景半透明 */}
+        <div className="absolute inset-0 bg-white/40" />
+
+        {/* 文章部分 */}
+        <div className="relative flex items-center flex-col">
+          <h1 className="text-[5.5rem] font-bold mt-20">文学部</h1>
+          <p className="mt-1 font-bold text-2xl font-serif tracking-tight">Faculty of Humanities</p>
+          <p className="mt-10 font-medium max-w-2xl">説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default FacultyPage;
+
+
+//ここは学部ページ /faculty/nameでアクセスできる
