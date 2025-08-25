01f4604ee763e0a7c2ddf996b2964d80616b0a86
kentf <kento.fukumoto0222@gmail.com>
2025-08-25 19:56:14 +0900

    faculty_headerの微調整

--- src/app/faculty/name/page.tsx
+++ src/app/faculty/name/page.tsx
@@ -3,19 +3,15 @@ import { Card, CardContent } from "@/components/ui/card";
 const FacultyPage = () => {
   return(
     <>
-      <div
-      className="relative h-screen w-full bg-cover bg-center"
-      style={{ backgroundImage: "url('/faculty_header.png')" }}
-      >
+      <div className="relative w-full bg-cover bg-center"
+           style={{ backgroundImage: "url('/faculty_header.png')" }}>
 
-        {/* 背景半透明 */}
-        <div className="absolute inset-0 bg-white/40" />
+        <div className="absolute inset-0 bg-white/60" />
 
-        {/* 文章部分 */}
         <div className="relative flex items-center flex-col">
-          <h1 className="text-[5.5rem] font-bold mt-20">文学部</h1>
+          <h1 className="text-[5.5rem] font-bold">文学部</h1>
           <p className="mt-1 font-bold text-2xl font-serif tracking-tight">Faculty of Humanities</p>
-          <p className="mt-10 font-medium max-w-2xl">説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文</p>
+          <p className="mt-10 font-normal max-w-2xl mb-10">説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文説明文</p>
         </div>
       </div>
     </>
