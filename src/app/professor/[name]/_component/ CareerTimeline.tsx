ab22a89b91a509b85eaf280c44be7c54db7465a8
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-09-02 18:13:58 +0900

    feat: CareerTimeline.tsx

---
+++ src/app/professor/[name]/_component/ CareerTimeline.tsx
@@ -0,0 +1,34 @@
+import {
+  Timeline,
+  TimelineItem,
+  TimelineTitle,
+  TimelineDescription,
+  TimelineHeader,
+  TimelineImage,
+} from "@/components/Timeline";
+
+export const CareerTimeline = ({
+  careerHistory,
+}: {
+  careerHistory: {
+    year: string;
+    event: string;
+    photo: string | null;
+  }[];
+}) => {
+  return (
+    <Timeline className="mt-8">
+      {careerHistory.map((item, index) => (
+        <TimelineItem key={index} isEven={index % 2 === 1}>
+          <TimelineHeader>
+            <TimelineTitle>{item.year}</TimelineTitle>
+          </TimelineHeader>
+          {item.event && (
+            <TimelineDescription>{item.event}</TimelineDescription>
+          )}
+          {item.photo && <TimelineImage src={item.photo} />}
+        </TimelineItem>
+      ))}
+    </Timeline>
+  );
+};
