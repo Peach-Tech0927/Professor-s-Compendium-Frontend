ec6f38431b654682e56574cafd3e9683cee88f86
ryolingo <ryota.matsumoto@eure.jp>
2025-09-03 17:53:57 +0900

    CardGridの実装

--- src/app/_component/FacultyCard.tsx
+++ src/app/_component/FacultyCard.tsx
@@ -11,18 +11,22 @@ type FacultyCardProps = {
   facultyDescription: string;
   FacultyImage: string;
   departments: Department[];
+  cardColor: string;
 }
 
 export default function FacultyCard({ 
   facultyName, 
   facultyDescription, 
   FacultyImage, 
-  departments 
+  departments,
+  cardColor
 }: FacultyCardProps) {
   return (
-    <div className="w-full lg:w-1/2 p-4 bg-white rounded-lg shadow-lg">
+    <div className="w-full p-4 bg-white rounded-lg shadow-lg h-full">
       <div className=" flex flex-col lg:flex-row rounded-lg">
-        <div className=" bg-[#5BA7E5] w-full lg:w-96 p-6 lg:p-8 flex items-center rounded-lg">
+      <div className="w-full lg:w-96 p-6 lg:p-8 flex items-center rounded-lg lg:h-86" 
+  style={{ backgroundColor: cardColor }}
+>
         <div className="flex-1">
           <h2 className="text-3xl font-bold mb-2 text-white">
             {facultyName}
@@ -44,7 +48,7 @@ export default function FacultyCard({
           </div>
           
           <div>
-            <div className="border-l-4 border-[#5BA7E5] pl-3 py-1">
+            <div className="border-l-4 pl-3 py-1" style={{ borderLeftColor: cardColor }}>
               <div className="font-medium text-gray-800">
                 {departments.map(d => d.name).join(' / ')}
               </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import FacultyCard from "./_component/FacultyCard";
+import FacultyCardGrid from "./_component/FacultyCardGrid";
 
 export default function Home() {
   return (
@@ -7,33 +7,7 @@ export default function Home() {
         <h1 className="text-5xl font-bold">成蹊大学教授図鑑</h1>
         <p className="mt-3">SEIKEI UNIVERSITY PROFESSOR CATALOG</p>
       </div>
-      <div className="min-h-screen py-12">
-      <div className="container mx-auto">
-        <FacultyCard 
-          facultyName="経済学部"
-          facultyDescription="現代社会の経済を多角的に学ぶ。"
-          FacultyImage="https://images.unsplash.com/photo-1562774053-701939374585?w=400&h=300&fit=crop"
-          departments={[
-            {
-              name: "経済経営学科",
-              englishName: "Department of Economics and Management"
-            },
-            {
-              name: "経済ファイナンス学科",
-              englishName: "Department of Economics and Finance"
-            },
-            {
-              name: "経済マネジメント学科",
-              englishName: "Department of Economics and Management"
-            },
-            {
-              name: "経済経済学科",
-              englishName: "Department of Economics and Management"
-            }
-          ]}
-        />
-      </div>
-    </div>
+      <FacultyCardGrid />
     </>
   );
 }
