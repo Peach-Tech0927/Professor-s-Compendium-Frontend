f40dfe84939d507c4d210cbeb8342acc633df68d
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-09-07 19:15:54 +0900

    feat: mockdataからデータを持ってきた

--- src/app/professor/[name]/_component/LessonListView.tsx
+++ src/app/professor/[name]/_component/LessonListView.tsx
@@ -2,14 +2,31 @@ import HeadLine from "@/components/HeadLine";
 import { BookOpen } from "lucide-react";
 import LessonCard from "./LessonCard";
 
-const LessonListView = () => {
+export type Lesson = {
+  PK: string;
+  SK: string;
+  courseName: string;
+  description: string;
+  learningPoints: string;
+  style: string;
+  relatedQualification: string | null;
+  targetGrade: string[];
+  courseCode: string;
+  syllabusLinkUrl: string;
+};
+
+type LessonListViewProps = {
+  lessons: Lesson[];
+};
+
+const LessonListView: React.FC<LessonListViewProps> = ({ lessons }) => {
   return (
     <div className="mt-10">
       <HeadLine icon={<BookOpen className="w-10 h-10 " />} title="担当授業" />
       <div className="mt-5 space-y-4">
-        <LessonCard />
-        <LessonCard />
-        <LessonCard />
+        {lessons.map((lesson) => (
+          <LessonCard key={lesson.SK} lesson={lesson} />
+        ))}
       </div>
     </div>
   );
--- src/app/professor/[name]/page.client.tsx
+++ src/app/professor/[name]/page.client.tsx
@@ -6,7 +6,7 @@ import ProfessorPersonalView, {
   ProfessorPersonalData,
 } from "./_component/ProfessorPersonalView";
 import SeminarInfoView from "./_component/SeminarInfoView";
-import LessonListView from "./_component/LessonListView";
+import LessonListView, { Lesson } from "./_component/LessonListView";
 
 type ClientProfessorPageProps = {
   xLink: string;
@@ -28,6 +28,7 @@ type ClientProfessorPageProps = {
     event: string;
     photo: string | null;
   }[];
+  lessons: Lesson[];
 };
 
 const ClientProfessorPage: React.FC<ClientProfessorPageProps> = ({
@@ -46,6 +47,7 @@ const ClientProfessorPage: React.FC<ClientProfessorPageProps> = ({
   seminarDescription,
   seminarDescriptionImage,
   careerHistory,
+  lessons,
 }) => {
   return (
     <>
@@ -87,14 +89,14 @@ const ClientProfessorPage: React.FC<ClientProfessorPageProps> = ({
             />
           </TabsContent>
           <TabsContent value="course" className="w-full">
-            <LessonListView />
+            <LessonListView lessons={lessons} />
           </TabsContent>
           <TabsContent value="seminor-info" className="w-full">
             <SeminarInfoView
-               seminarName={seminarName}
-               seminarDescription={seminarDescription}
-               seminarDescriptionImage={seminarDescriptionImage}
-             />
+              seminarName={seminarName}
+              seminarDescription={seminarDescription}
+              seminarDescriptionImage={seminarDescriptionImage}
+            />
           </TabsContent>
           <TabsContent value="personal" className="w-full">
             <ProfessorPersonalView personalData={personalData} />
