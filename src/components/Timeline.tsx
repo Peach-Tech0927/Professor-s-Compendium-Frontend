b7215ef03ea9f46367142faa16986bce61ff182c
unknown <sohgo.kobatyan@icloud.com>
2025-09-10 19:29:24 +0900

    1個前のコミット修正

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -2,14 +2,6 @@ import { cn } from "@/lib/utils";
 import React from "react";
 import Image from "next/image";
 
-//export type TimelineItemType = {
-//  year: string;
-//  event: string;
-//  photo: string | null;
-//  period:string;
-//  task:string;
-//};
-
 const Timeline = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
