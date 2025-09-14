43f0594e4dd20fc22ce6b3b83f0be53d0a089f3b
taroimokun <runa.osada.26@outlook.jp>
2025-09-14 21:34:09 +0900

    ゼミ情報のUIをfigma通りにする

--- src/app/professor/[name]/_component/SeminarInfoView.tsx
+++ src/app/professor/[name]/_component/SeminarInfoView.tsx
@@ -50,26 +50,18 @@ const SeminarInfoView = ({
           </p>
         </div>
       </div>
-      <div className="mt-10">
-        <div className="flex mx-4 lg:mx-50 gap-2 items-center">
-          <p className="text-lg lg:text-2xl font-bold">年間スケジュール</p>
-        </div>
-        <div className="mt-30">
-          <SeminarSchedule seminarSchedule={seminarSchedule} />
-        </div>
-      </div>
       <div className="flex mx-4 lg:mx-50 gap-2 items-center mt-10">
-        <h1 className="text-lg lg:text-2xl font-bold">研究の流れ</h1>
+        <h1 className="text-lg lg:text-2xl font-bold">研究の進め方</h1>
       </div>
       <div className="flex flex-col lg:flex-row gap-5 lg:gap-10 mt-5">
         <div className="flex-shrink-0 order-2 lg:order-2">
           <div className="lg:pr-50">
             <Image
-                src="/ProfileExample.png"
-                alt="researchProcessImage"
-                width={500}
-                height={400}
-                className="object-cover w-full max-w-[300px] lg:max-w-[500px] h-[240px] mx-auto"
+              src="/ProfileExample.png"
+              alt="researchProcessImage"
+              width={500}
+              height={400}
+              className="object-cover w-full max-w-[300px] lg:max-w-[500px] h-[240px] mx-auto"
             />
           </div>
         </div>
@@ -77,6 +69,14 @@ const SeminarInfoView = ({
           <p>
             このゼミでは、平安時代の文学であればどの作品を研究しても構いません。「落窪物語」「伊勢物語」「竹取物語」「枕草子」「とりかへばや物語」など、多様な作品が研究対象となっています。国宝の「源氏物語絵巻」や漫画「あさきゆめみし」など、「源氏物語」の絵画化作品を取り上げる人もいます。とはいえ、人気があるのはやはり「源氏物語」。或る登場人物--例えば紫の上に焦点を当てたり、或る場面--例えば光源氏と紫の上が出会う場面を丁寧に読み解いたり、或る物--例えば牛車に注目して作品を分析したり、といったように研究テーマは多様です。
           </p>
+        </div>
+      </div>
+      <div className="mt-10">
+        <div className="flex mx-4 lg:mx-50 gap-2 items-center">
+          <p className="text-lg lg:text-2xl font-bold">年間スケジュール</p>
+        </div>
+        <div className="mt-30">
+          <SeminarSchedule seminarSchedule={seminarSchedule} />
         </div>
       </div>
       <div className="flex justify-center items-start mt-10 gap-50">
