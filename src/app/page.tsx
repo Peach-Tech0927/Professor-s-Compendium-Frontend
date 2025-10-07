b3d0c7431d53f2062d4d51ee079b0d5f50607a42
yoshiddddd <yoshidakerapo@gmail.com>
2025-10-07 13:24:30 +0900

    create footer@

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import FacultyCardGrid from "./_component/FacultyCardGrid";
+import Footer from "./_component/Footer";
 
 export default function Home() {
   return (
-    <>
-      <div className="flex items-center content-center flex-col mt-20">
-        <h1 className="text-5xl font-bold">成蹊大学教授図鑑</h1>
-        <p className="mt-3">SEIKEI UNIVERSITY PROFESSOR CATALOG</p>
+    <div className="min-h-screen flex flex-col">
+      <div className="flex-1">
+        <div className="flex items-center content-center flex-col mt-20">
+          <h1 className="text-5xl font-bold">成蹊大学教授図鑑</h1>
+          <p className="mt-3">SEIKEI UNIVERSITY PROFESSOR CATALOG</p>
+        </div>
+        <FacultyCardGrid />
       </div>
-      <FacultyCardGrid />
-    </>
+      <Footer />
+    </div>
   );
 }
