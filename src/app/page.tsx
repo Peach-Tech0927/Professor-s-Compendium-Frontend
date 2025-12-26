0b29d722ec5ced763e00260315be807b6aa2275d
yoshiddddd <yoshidakerapo@gmail.com>
2025-12-26 10:34:16 +0900

    dynamic fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import FacultyCardGrid from "./_component/FacultyCardGrid";
 import Footer from "./_component/Footer";
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
